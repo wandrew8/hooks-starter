d304d4be517667ac1541d1d9f9dd29a89397da89
Andrew Weiss <wandrew8@gmail.com>
2020-10-01 13:07:13 -0700

    Updates

---
+++ precache-manifest.cdd8785e0defb14eb3f1e0e44d57b161.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a35cb13eeddf74f5a4e",
+    "url": "/hooks-starter/static/css/main.cd89009a.chunk.css"
+  },
+  {
+    "revision": "7a35cb13eeddf74f5a4e",
+    "url": "/hooks-starter/static/js/main.7a35cb13.chunk.js"
+  },
+  {
+    "revision": "50a5b037d1e0169582b6",
+    "url": "/hooks-starter/static/js/runtime~main.50a5b037.js"
+  },
+  {
+    "revision": "6fee35e16db316018c9f",
+    "url": "/hooks-starter/static/js/2.6fee35e1.chunk.js"
+  },
+  {
+    "revision": "276cca2614beb4f3226e5b7de57f9642",
+    "url": "/hooks-starter/index.html"
+  }
+];
